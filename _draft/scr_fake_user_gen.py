1462d0d5cb0ebd744c0f721bfb66c4aed6c610e4
Alex <ax.dev@yahoo.com>
2023-06-17 22:41:40 +0300

    name prefix fix

--- _draft/scr_fake_user_gen.py
+++ _draft/scr_fake_user_gen.py
@@ -5,8 +5,7 @@ from faker import Faker
 
 
 fake = Faker('ru_RU')
-cost = 2
-
+cost = 2000
 
 def latinizator(letter):
     legend = {' ': ' ', ',': ',', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': 'y', 'ы': 'y', 'ь': "'", 'э': 'e', 'ю': 'yu', 'я': 'ya', 'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo', 'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ъ': 'Y', 'Ы': 'Y', 'Ь': "'", 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'}
@@ -29,5 +28,9 @@ def ikato_namer(name):
 
 for _ in range(cost):
     name = fake.name()
-    print(name)
-    print(f"| ikato = {ikato_namer(name)} | Имя :: {name} | Name :: {latinizator(name)} | Email :: {email_namer(name)} | Адрес :: {fake.address()} | IP :: {fake.ipv4_private()} |")
+    print(f"Name :: {name} // {len(name.split())}")
+    
+    if len(name.split()) == 4:
+        name = name.split()[1] + " " + name.split()[2] + " " + name.split()[3]
+    
+    print(f"| ikato = {ikato_namer(name)} | Имя :: {name} | Name :: {latinizator(name)} | date :: {fake.date()} | Job :: {fake.job()} | Email :: {email_namer(name)} | Адрес :: {fake.address()} | IP :: {fake.ipv4_private()} |")
