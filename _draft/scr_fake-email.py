317b1af4850685cbcf19e74724bc16940dcaf431
Alex <ax.dev@yahoo.com>
2023-06-17 21:32:01 +0300

    emailer add

--- _draft/scr_fake-email.py
+++ _draft/scr_fake-email.py
@@ -2,89 +2,27 @@ import os
 from sys import argv
 import random
 
+
+legend = {' ': ' ', ',': ',', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': 'y', 'ы': 'y', 'ь': "'", 'э': 'e', 'ю': 'yu', 'я': 'ya', 'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo', 'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ъ': 'Y', 'Ы': 'Y', 'Ь': "'", 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'}
+
 def latinizator(letter, dic):
     for i, j in dic.items():
         letter = letter.replace(i, j)
     return letter
 
-legend = {
-' ':' ',
-',':',',
-'а':'a',
-'б':'b',
-'в':'v',
-'г':'g',
-'д':'d',
-'е':'e',
-'ё':'yo',
-'ж':'zh',
-'з':'z',
-'и':'i',
-'й':'y',
-'к':'k',
-'л':'l',
-'м':'m',
-'н':'n',
-'о':'o',
-'п':'p',
-'р':'r',
-'с':'s',
-'т':'t',
-'у':'u',
-'ф':'f',
-'х':'h',
-'ц':'c',
-'ч':'ch',
-'ш':'sh',
-'щ':'shch',
-'ъ':'y',
-'ы':'y',
-'ь':"'",
-'э':'e',
-'ю':'yu',
-'я':'ya',
+def email_namer(name):
 
-'А':'A',
-'Б':'B',
-'В':'V',
-'Г':'G',
-'Д':'D',
-'Е':'E',
-'Ё':'Yo',
-'Ж':'Zh',
-'З':'Z',
-'И':'I',
-'Й':'Y',
-'К':'K',
-'Л':'L',
-'М':'M',
-'Н':'N',
-'О':'O',
-'П':'P',
-'Р':'R',
-'С':'S',
-'Т':'T',
-'У':'U',
-'Ф':'F',
-'Х':'H',
-'Ц':'Ts',
-'Ч':'Ch',
-'Ш':'Sh',
-'Щ':'Shch',
-'Ъ':'Y',
-'Ы':'Y',
-'Ь':"'",
-'Э':'E',
-'Ю':'Yu',
-'Я':'Ya',
-}
+    latino_word = latinizator(name, legend).lower()
 
-mail_hosts = ['gmail.com', 'yahoo.com', 'outlook.com', 'mail.ru', 'yandex.ru']
+    mail_hosts = ['gmail.com', 'yahoo.com', 'outlook.com', 'mail.ru', 'yandex.ru']
 
-#word = "Колесникова Александра Филипповна // ст. Кандалакша, алл. 40 лет Победы, д. 81 к. 11, 169684"
+    email = latino_word.split()[0] + "." + latino_word.split()[1][0] + latino_word.split()[2][0] + "@" + random.choice(mail_hosts)
+    
+    return(email)
+
 word = "Колесникова Александра Филипповна"
 
 print(latinizator(word, legend))
+print(email_namer(word))
 
-email = latinizator(word, legend).lower().replace(' ', '_') + "@" + random.choice(mail_hosts)
-print(email)
+#print(legend)
